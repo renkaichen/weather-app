ff5642ed22318827086a3afef3d2526087427b23
Kai Chen <kchen0323@gmail.com>
2025-03-07 17:38:55 -0500

    Style search bar

--- src/homepage.js
+++ src/homepage.js
@@ -4,24 +4,30 @@ function initalize() {
     initalizeSearchbar();
 }
 
-function initalizeSearchbar() {
-    const content = document.getElementById("content");
-    const searchBar = document.createElement("input");
-    const button = document.createElement("button");
-
-    searchBar.type = "text";
-    searchBar.placeholder = "Enter a location";
-    searchBar.setAttribute("id", "search");
-    button.textContent = "Search";
-    button.addEventListener("click", () => search());
-
-    content.appendChild(searchBar);
-    content.appendChild(button);
-}
-
 function search() {
     const input = document.getElementById("search").value;
     getWeatherData(input);
+}
+
+function initalizeSearchbar() {
+    const content = document.getElementById("content");
+    const input = document.createElement("input");
+    const button = document.createElement("button");
+    const searchbar = document.createElement("div");
+
+    
+    input.type = "text";
+    input.placeholder = "Enter a location";
+    input.setAttribute("id", "search");
+
+    button.textContent = "Search";
+    button.addEventListener("click", () => search());
+
+    searchbar.setAttribute("id", "searchbar");
+    searchbar.appendChild(input);
+    searchbar.appendChild(button);
+
+    content.appendChild(searchbar);
 }
 
 export default initalize;
