7bc2216eb48da999c574032895e08ce05d1e5292
Kai Chen <kchen0323@gmail.com>
2025-03-15 17:10:10 -0400

    Add movement between favorites

--- src/homepage.js
+++ src/homepage.js
@@ -1,5 +1,8 @@
 import getWeatherData from "./weatherDisplay";
+
+
 let favoriteLocations = [];
+let currentFavoriteIndex = null;
 
 function loadSavedData() {
     if (localStorage.getItem('favoriteLocations') != undefined) {
@@ -15,6 +18,7 @@ function initalize() {
 
 function search() {
     const input = document.getElementById("search").value;
+    console.log(input);
     document.getElementById("search").value = '';
     document.getElementById("search").style.backgroundColor = "rgba(0,0,0,0%)";
     document.getElementById("favorite").style.display = "none";
@@ -52,16 +56,28 @@ function initalizeFavorites() {
     favorite.setAttribute("id", "favorite");
 
     const location = document.createElement("h4");
-    const currentTemp = document.createElement("h1");
-    const currentCondition = document.createElement("h2");
 
-    favorite.append(location,currentTemp, currentCondition);
+    favorite.append(location);
     content.prepend(favorite);
 
     if (favoriteLocations.length == 0) {
         location.textContent = "No Favorites Yet"
     } else {
-        displayFavorite(0);
+        currentFavoriteIndex = 0;
+
+        if (favoriteLocations.length != 1) {
+            const rightButton = document.createElement("div");
+            rightButton.setAttribute("id","rightButton");
+            rightButton.addEventListener("click",() => nextFavorite());
+
+            const leftButton = document.createElement("div");
+            leftButton.setAttribute("id","leftButton");
+            leftButton.addEventListener("click",() => prevFavorite());
+            
+            content.parentElement.appendChild(rightButton);
+            content.parentElement.appendChild(leftButton);
+        }
+        displayFavorite(currentFavoriteIndex);
     }
 }
 
@@ -77,6 +93,21 @@ export function addFavorite(location) {
 export function unfavorite(location) {
     favoriteLocations.splice(favoriteLocations.indexOf(location), 1);
     localStorage.setItem('favoriteLocations', JSON.stringify(favoriteLocations));
+    if (document.getElementById("favorite").style.display != "none") {
+        if (favoriteLocations.length == 1) {
+            nextFavorite();
+            const background = document.getElementById("background");
+            background.removeChild(document.getElementById("rightButton"));
+            background.removeChild(document.getElementById("leftButton"));
+        } else if (favoriteLocations.length != 0) {
+            nextFavorite();
+        } else {
+            document.getElementById("favorite").innerHTML = '';
+            const text = document.createElement("h4");
+            text.textContent = "No Favorites Yet";
+            document.getElementById("favorite").appendChild(text);
+        }
+    }
 }
 
 export function checkFavorites(location) {
@@ -86,5 +117,23 @@ export function checkFavorites(location) {
         return false;
     }
 }
+
+function nextFavorite() {
+    currentFavoriteIndex = (currentFavoriteIndex + 1) % favoriteLocations.length;
+    const favoriteHolder = document.getElementById("favorite");
+    favoriteHolder.innerHTML = '';
+    displayFavorite(currentFavoriteIndex);
+}
+
+function prevFavorite() {
+    currentFavoriteIndex--;
+    if (currentFavoriteIndex < 0) {
+        currentFavoriteIndex = favoriteLocations.length - 1;
+    }
+    const favoriteHolder = document.getElementById("favorite");
+    favoriteHolder.innerHTML = '';
+    displayFavorite(currentFavoriteIndex);
+}
+
 
 export default initalize;
